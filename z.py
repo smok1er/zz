d13b79d5450d06c6f43ed22cd06f7abffbfecdf6
smok1er <107242195+smok1er@users.noreply.github.com>
2022-07-24 00:15:36 +0300

    Add files via upload

---
+++ z.py
@@ -0,0 +1,51 @@
+import requests,random
+F = '\033[2;32m' #اخضر
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#ازرق
+C = '\033[2;35m' #وردي
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ا
+id = input(B+'Enter Your Id => ')
+tok = input(B+'Enter Your Token => ')
+print(Z+'='*58)
+xz ='AASSDDFFGGHHJJKKLLQQWWEERRTTYYUUIIOOPPMMNNBBVVCXXZZ'
+H = '1234567890'
+o ='_'
+while True:
+	v = str("".join(random.choice(xz)for i in range(1)))
+	y = str("".join(random.choice(xz)for i in range(1)))	
+	g = str("".join(random.choice(xz)for i in range(1)))
+	i = str("".join(random.choice(H)for i in range(1)))
+	Q = v+o+g+o+y
+	B = v+o+y+o+g
+	C = y+o+g+o+v
+	U = g+o+y+o+v
+	K =g+o+i+o+v
+	a =y+o+i+o+v
+	BAS = [Q,B,C,U,K,a]
+	Us = str(random.choice(BAS))
+	z = str("".join(random.choice(xz)for i in range(1)))
+	L = str("".join(random.choice(xz)for i in range(1)))	
+	J = str("".join(random.choice(xz)for i in range(1)))
+	W = L+o+z+o+J
+	E = J+o+z+o+L
+	T = L+o+J+o+z
+	N = z+o+L+o+J
+	BASHA = [W,E,T,N]
+	USER = str(random.choice(BASHA))
+	A = [Us,USER]
+	USERNAME = str(random.choice(A))
+	
+	url=f"https://t.me/{USERNAME}"
+	req = requests.get(url).text
+	if '"tgme_username_link"' in req:
+		print(F+'Done  | ' + USERNAME )
+		tlg = (f'''https://api.telegram.org/bot{tok}/sendMessage?chat_id={id}&text=@{USERNAME} + \n''')
+		i = requests.post(tlg)		
+		
+	else:
+		print(Z+'ERORR  | '+USERNAME)
+		
+		
